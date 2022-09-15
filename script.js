b99c64c012939225150c678159774bf2130e848e
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-09-15 08:43:09 -0700

    tow homework js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ console.log('Z '+'='+' '+z)
 // this is one task js language 
 // line 1 ,2 and varibles
 // line 4 print this varibles in browser
+// -----------------------------------------------
+//-------homework tow-----------------------------
+
+
+document.getElementById("out").onclick= function(){
+    let input1 = document.getElementById("in").value;
+    // console.log("hello"+input1);
+    document.getElementById('ww').innerHTML = input1/5*9+32;
+}
